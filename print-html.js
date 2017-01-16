43605cef0a3bf93d0f6f72b2121208451e7ebe34
MichaelMini <mcho911@gmail.com>
2017-01-16 20:18:17 +0000

    Step 3 completed - Printing with Parameters

---
+++ print-html.js
@@ -0,0 +1,30 @@
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+};
+
+function getAndPrintHTML (option) {
+  var https = require('https');
+
+  /* Add your code here */
+  https.get(option, function (response) {
+    let contents = '';
+
+  	response.setEncoding('utf8');
+
+  	response.on('data', function(data) {
+      contents += data;
+      // console.log(contents);
+  		// console.log(data + '\n');
+  	});
+
+  	response.on('end', function() {
+  	  console.log(contents);
+  	});
+
+
+  });
+
+}
+
+getAndPrintHTML(requestOptions);
