ece41b95553c6a309f05dc02525356059372ac03
MichaelMini <mcho911@gmail.com>
2017-01-16 20:50:30 +0000

    correct step3 in line-3

--- print-html.js
+++ print-html.js
@@ -1,6 +1,6 @@
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
+  path: '/http-examples/step3.html'
 };
 
 function getAndPrintHTML (option) {
@@ -9,22 +9,16 @@ function getAndPrintHTML (option) {
   /* Add your code here */
   https.get(option, function (response) {
     let contents = '';
-
   	response.setEncoding('utf8');
-
   	response.on('data', function(data) {
       contents += data;
       // console.log(contents);
   		// console.log(data + '\n');
   	});
-
   	response.on('end', function() {
   	  console.log(contents);
   	});
-
-
   });
-
 }
 
 getAndPrintHTML(requestOptions);
