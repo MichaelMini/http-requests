9124e12704318c7029357b8bb57940bb5a07117e
MichaelMini <mcho911@gmail.com>
2017-01-16 20:48:09 +0000

    correct step2 in line-6

--- print.js
+++ print.js
@@ -3,28 +3,20 @@ function getAndPrintHTML () {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   /* Add your code here */
   https.get(requestOptions, function (response) {
     let contents = '';
-
   	response.setEncoding('utf8');
-
   	response.on('data', function(data) {
       contents += data;
-      // console.log(contents);
-  		// console.log(data + '\n');
   	});
-
   	response.on('end', function() {
   	  console.log(contents);
   	});
-
-
   });
-
 }
 
 getAndPrintHTML();
